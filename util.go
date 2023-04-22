f2af31817df694e03fbc0a107d36098c6b98a446
emschu <emschu@mailbox.org>
2023-04-22 15:14:23 +0200

    fix license headers
    Signed-off-by: emschu <emschu@mailbox.org>

--- util.go
+++ util.go
@@ -1,6 +1,5 @@
-//
 // oerc, alias oer-collector
-// Copyright (C) 2021 emschu[aet]mailbox.org
+// Copyright (C) 2021-2023 emschu[aet]mailbox.org
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -146,6 +145,7 @@ func getHTTPProxy() func(r *http.Request) (*url.URL, error) {
 }
 
 // central method to do http get requests in this application, retry count relates to (idle) connection count!
+//
 //goland:noinspection GoDeferInLoop
 func doGetRequest(target string, requestHeaders map[string]string, retries uint) (*string, error) {
 	// global http client
